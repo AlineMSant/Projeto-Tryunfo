bd318606aba3721570f183146e09ebb3bbbf4944
AlineMSant <alinems4120@gmail.com>
2023-01-27 19:21:41 -0300

    requisito 4 incluir descrições para renderizar

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,13 +14,31 @@ class Card extends React.Component {
       cardTrunfo } = this.props;
     return (
       <div>
-        <p data-testid="name-card">{ cardName }</p>
+        <p data-testid="name-card">
+          Nome
+          { cardName }
+        </p>
         <img data-testid="image-card" src={ cardImage } alt={ cardName } />
-        <p data-testid="description-card">{ cardDescription }</p>
-        <p data-testid="attr1-card">{ cardAttr1 }</p>
-        <p data-testid="attr2-card">{ cardAttr2 }</p>
-        <p data-testid="attr3-card">{ cardAttr3 }</p>
-        <p data-testid="rare-card">{ cardRare }</p>
+        <p data-testid="description-card">
+          Descrição
+          { cardDescription }
+        </p>
+        <p data-testid="attr1-card">
+          Attr1
+          { cardAttr1 }
+        </p>
+        <p data-testid="attr2-card">
+          Attr2
+          { cardAttr2 }
+        </p>
+        <p data-testid="attr3-card">
+          Attr3
+          { cardAttr3 }
+        </p>
+        <p data-testid="rare-card">
+          Raridade
+          { cardRare }
+        </p>
         <div>{cardTrunfo === true && <p data-testid="trunfo-card">Super Trunfo</p>}</div>
       </div>
     );
